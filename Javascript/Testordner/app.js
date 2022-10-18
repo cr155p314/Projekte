ced45e19cdb3032eff1ee4cc1a3810b3416a1dc7
cr155p314 <christian.putschky@web.de>
2022-10-18 18:25:06 +0200

    .

--- Javascript/Testordner/app.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  let person = prompt("Please enter your name", "Harry Potter");
-  if (person != null) {
-    document.getElementById("demo").innerHTML =
-    "Hello " + person + "! How are you today?";
-  }
-}
